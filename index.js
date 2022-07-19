ae56da8504a46a5efa9412baa44963deddb07e7e
larimar1212 <larimarfondeur@gmail.com>
2022-07-19 11:11:46 -0400

    commit tree 5a781c541234416dac8e01288fcdaa7b5386139c

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name); // adds to the end
+}
+
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name); //a adds to the beg
+}
+
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name) // removes las eleemnt
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name) // remove first element
+}
+
+function appendCat(name) {
+   const copyCats = [...cats, name];
+   return copyCats
+
+}
+//function appendCat(name) {
+    //  cats.push(name)
+    //r
+    //}
+    
+/*function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+
+
+function prependCat(name) {
+    const newCats = [name, ...cats];
+    return newCats
+}
+
+function removeLastCat(name) {
+   const newCats =  cats.slice(0, cats.length - 1);
+   return newCats
+}
+
+function removeFirstCat(name) {
+    const babyCats = cats.slice(1);
+    return babyCats
+} */
